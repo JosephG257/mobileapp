ae590b69d43e0b491ca2181f94424433dac46309
Josephine Garner <josephine.garner@tooeleschools.org>
2019-08-30 09:35:56 -0600

    yeet

---
+++ ShowTime.java
@@ -0,0 +1,13 @@
+// Joseph Garner
+// 8/30/2019
+
+import java.util.Calendar;
+import static java.lang.System.out;
+
+public class ShowTime 
+{
+public static void main(String[] args) 
+   {
+	out.println(Calendar.getInstance().getTime());
+   }
+}
