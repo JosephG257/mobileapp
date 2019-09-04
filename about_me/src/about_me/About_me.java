4d507a2ee1132830377e5c4db96bb27c1a578888
Josephine Garner <josephine.garner@tooeleschools.org>
2019-09-04 10:08:18 -0600

    example projects first push

---
+++ about_me/src/about_me/About_me.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package about_me;
+
+/**
+ * @author joseph.garner
+ *
+ */
+public class About_me {
+
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Im Joseph Garner");
+		System.out.println("1st Period-Color Guard");
+		System.out.println("2nd-Math Decsion Making");
+		System.out.println("3rd-Fin Lit");
+		System.out.println("4th-Creative Writing");
+		System.out.println("5th-Sociology");
+		System.out.println("6th/7th-Mobile App");
+		System.out.println("8th-ASL 3rd year");
+		System.out.println("9th-Credit Recovery");
+		System.out.println("10th-Int. Chorus");
+		System.out.println("This is my first semesters classes for my senior year");
+		
+	}
+
+}
---
+++ example _projects/src/joseph_example_projects/Hello_world.java
@@ -0,0 +1,14 @@
+//JosephGarner 9/19
+package joseph_example_projects;
+
+public class Hello_world {
+
+	public Hello_world() {
+		System.out.println("Hello There World");
+	}
+	
+	public static void main(String[] args) {
+		new Hello_world();
+	}
+
+}
